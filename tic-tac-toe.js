5efdd9e72cff1382754c574377027947e1198cfb
Jaheim Dazzell <bennyjaheim@gmail.com>
2025-10-14 23:33:42 -0400

    Added initial starter files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,7 @@
-// Use window.onload instead of DOMContentLoaded to ensure everything is ready
-window.onload = function() {
-    console.log('Page fully loaded - script running');
-    
-    // Add square classes to board divs
+document.addEventListener('DOMContentLoaded', function() {
     const boardSquares = document.querySelectorAll('#board div');
-    console.log('Found', boardSquares.length, 'squares');
     
-    boardSquares.forEach((square, index) => {
+    boardSquares.forEach(square => {
         square.classList.add('square');
-        console.log('Added square class to div', index);
     });
-    
-    // Make New Game button open YouTube video
-    const newGameBtn = document.querySelector('.btn');
-    console.log('Button found:', newGameBtn);
-    
-    newGameBtn.addEventListener('click', function() {
-        console.log('Button clicked! Opening YouTube...');
-        window.open('https://www.youtube.com/watch?v=r8uBiiig7tI', '_blank');
-    });
-    
-    console.log('Setup complete');
-};
+});
